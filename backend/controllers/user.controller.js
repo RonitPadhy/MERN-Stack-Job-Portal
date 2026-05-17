91c1a81717b88008095e3d680b9f7e1d6fa66797
Ronit Padhy <rohanfbbk1@gmail.com>
2026-05-17 15:17:58 +0530

    added the logout api

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import UserModel from "../models/user.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 class UserController {
   static async Register(req, res) {
@@ -40,6 +41,79 @@ class UserController {
         message: "Internal Server Error",
         success: false,
       });
+    }
+  }
+
+  static async login(req, res) {
+    try {
+      const { email, password, role } = req.body;
+      if (!email || !password || !role) {
+        return res
+          .status(400)
+          .json({ message: "Please enter all the fields", success: false });
+      }
+
+      const user = await UserModel.findOne({ email });
+      if (!user) {
+        return res.status(404).json({
+          message: "User not found...Please register",
+          success: false,
+        });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ message: "Invalid Credentials", success: false });
+      }
+
+      if (role !== user.role) {
+        return res.status(400).json({
+          message: "Account does not exist with the current role",
+          success: false,
+        });
+      }
+
+      const tokenData = {
+        _id: user._id,
+        role: user.role,
+      };
+
+      const token = jwt.sign(tokenData, process.env.SECRET_KEY, {
+        expiresIn: "1d",
+      });
+
+      return res
+        .status(200)
+        .cookie("token", token, {
+          httpOnly: true,
+          sameSite: "strict",
+          maxAge: 1 * 24 * 60 * 60 * 1000,
+        })
+        .json({
+          message: "User logged in Succesfully",
+          success: true,
+        });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
+  }
+
+  static async logout(req, res) {
+    try {
+      return res
+        .status(200)
+        .cookie("token", "", { httpOnly: true, sameSite: "strict", maxAge: 0 })
+        .json({
+          message: "User logged out successfully",
+          success: true,
+        });
+    } catch (err) {
+      return res
+        .status(500)
+        .json({ message: "Error Logging out", success: false });
     }
   }
 }
